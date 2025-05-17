b83b8c8a154c49d67871870da6fe76b539af0b24
coder7475 <robiulhossain7475@gmail.com>
2025-05-17 09:04:57 +0600

    feat: learn varaible initialization

---
+++ learn_cpp/1_var_init.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int width {5};          // define variable width and initialize it with value 5
+
+  cout << width << endl;  // prints value 5
+
+  return 0;
+}
