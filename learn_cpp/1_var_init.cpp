473e8a49a4ecaed7c5492b51ec171c49eb19c4f9
coder7475 <robiulhossain7475@gmail.com>
2025-05-17 09:10:37 +0600

    feat: learn different form of initialization

--- learn_cpp/1_var_init.cpp
+++ learn_cpp/1_var_init.cpp
@@ -5,6 +5,22 @@ int main() {
   int width {5};          // define variable width and initialize it with value 5
 
   cout << width << endl;  // prints value 5
+  
+  // Different forms of initialization
+  int a;   // deafult-initialization (no initializer)
+
+  // Traditional initialization
+  int b = 5;     // copy initialization
+  int c (6);     // direct initialization
+                 
+  // Modern initialization forms (preferred)
+  int d { 7 };    // direct list initialization
+  int e { };      // value initialization
+
+  cout << b << endl;
+  cout << c << endl;
+  cout << d << endl;
+  cout << e << endl;
 
   return 0;
 }
