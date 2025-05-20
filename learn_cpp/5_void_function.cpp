932ab8a8a80f726f3f82cde7ba7cae51d0bcaa84
coder7475 <robiulhossain7475@gmail.com>
2025-05-20 10:03:26 +0600

    feat: learn void function

---
+++ learn_cpp/5_void_function.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// void means the function does not return a value to the caller
+void printHi() { cout << "Hi" << "\n"; }
+
+int main() {
+  printHi();
+  return 0;
+}
