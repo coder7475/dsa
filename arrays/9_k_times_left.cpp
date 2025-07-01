bb68ae1c964553d9700808813c49b7e681fc7f01
coder7475 <robiulhossain7475@gmail.com>
2025-07-01 10:32:40 +0600

    feat: k times left rotation

---
+++ arrays/9_k_times_left.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  int *a = new int[n];
+
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+
+  int k;
+  cin >> k;
+
+  // logic
+  int *ans = new int[n];
+
+  for (int i = 0; i < n; i++) {
+    int idx = (i - k + n) % 5;
+    ans[idx] = a[i];
+  }
+
+  // output
+  for (int i = 0; i < n; i++)
+    cout << ans[i] << " ";
+
+  cout << "\n";
+
+  return 0;
+}
