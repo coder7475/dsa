64ecd48f78dde94dca1d48646fa26fb74e818aa8
coder7475 <robiulhossain7475@gmail.com>
2025-07-10 11:23:39 +0600

    feat: creat matrix input and output

---
+++ matrix_temp.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  // input
+  int n, m;
+
+  cin >> n >> m;
+
+  vector<vector<int>> matrix(n, vector<int>(m));
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> matrix[i][j];
+    }
+  }
+
+  // Operation
+
+  // output
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
+      cout << matrix[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
