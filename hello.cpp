dec93b1e4650ae12290a868763784a014fd58b3b
coder7475 <robiulhossain7475@gmail.com>
2025-07-19 10:42:38 +0600

    feat: firt boilerplate using gencpp

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int main(){
-   cout << "Hello, world!" << endl;
-   return 0;
+int main() {
+    int t;
+    vector<int> v;
+    // Your code here
+
+    return 0;
 }
