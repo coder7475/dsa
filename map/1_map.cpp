f4a9935b74cd470438c6d982f52830abfb598203
coder7475 <robiulhossain7475@gmail.com>
2025-07-26 21:40:33 +0600

    feat: learn map

---
+++ map/1_map.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+  map<int, string> m;
+
+  // Insert values
+  m[1] = "Apple";
+  m[2] = "Banana";
+  m[5] = "Cherry";
+  m[100] = "Orange";
+
+  // Access and print
+  for (const auto &pair : m) {
+    std::cout << pair.first << " => " << pair.second << "\n";
+  }
+
+  return 0;
+}
