799ecaac2d7660f4daaad9ae6eca84cb37b860e5
coder7475 <robiulhossain7475@gmail.com>
2025-08-07 11:20:13 +0600

    feat: learn map

--- map/1_map.cpp
+++ map/1_map.cpp
@@ -17,8 +17,24 @@ int main() {
   }
 
   m[100] = "Law";
-  cout << "100 => " << m[100] << endl; // output: orange
-  cout << m[23] << endl;               // output: " "
+  // cout << "100 => " << m[100] << endl; // output: orange
+  //   cout << m[23] << endl;               // output: " "
+  //
+  cout << "Updated New value\n";
+  for (const auto &[key, value] : m) {
+    cout << key << " => " << value << "\n";
+  }
+
+  // map in descending order
+  map<int, string, greater<int>> descMap;
+
+  descMap[1] = "Nathan";
+  descMap[99] = "Lily";
+  descMap[100] = "Rody";
+  cout << "Descending Order Map: \n";
+  for (const auto &[key, value] : descMap) {
+    cout << key << " => " << value << "\n";
+  }
 
   return 0;
 }
